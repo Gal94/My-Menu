041b284feb7184973c29e681847692795865d8b7
Gal <yacobi.gal94@gmail.com>
2021-03-19 11:24:21 +0200

    scroll detection for navbar styles

--- client/src/components/Navbar/Navbar.component.jsx
+++ client/src/components/Navbar/Navbar.component.jsx
@@ -1,8 +1,38 @@
+import { useEffect, useState } from 'react';
 import { NavbarContainer } from './Navbar.styles';
 
+//TODO Add content and appropriate styles to navbar
+
+let listener;
+
 const Navbar = () => {
+    const [isTop, setIsTop] = useState(true);
+
+    useEffect(() => {
+        listener = document.addEventListener('scroll', (e) => {
+            // get how much have been scrolled from top
+            let scrolled = document.scrollingElement.scrollTop;
+
+            // apply styles
+            if (scrolled >= 10) {
+                if (isTop !== false) {
+                    setIsTop(false);
+                }
+            } else {
+                if (isTop !== true) {
+                    setIsTop(true);
+                }
+            }
+        });
+
+        // upon unmounting remove listener
+        return () => {
+            document.removeEventListener('scroll', listener);
+        };
+    }, [isTop]);
+
     return (
-        <NavbarContainer>
+        <NavbarContainer isTransparent={isTop}>
             Its a navbar thats needs to be implemented
         </NavbarContainer>
     );
--- client/src/components/Navbar/Navbar.styles.js
+++ client/src/components/Navbar/Navbar.styles.js
@@ -1,4 +1,22 @@
-import Styled from 'styled-components';
+import Styled, { css } from 'styled-components';
+
+const backgroundStyle = (props) => {
+    if (props.isTransparent) {
+        return transparentCss;
+    } else {
+        return coloredCss;
+    }
+};
+
+export const transparentCss = css`
+    background-color: transparent;
+`;
+
+export const coloredCss = css`
+    background-color: #47c225;
+    box-shadow: 0 2px 10px 1px #297535;
+    transition: all 0.5s linear;
+`;
 
 export const NavbarContainer = Styled.nav`
     position: fixed;
@@ -6,8 +24,7 @@ export const NavbarContainer = Styled.nav`
     left: 0;
     width: 100%;
     height: 3rem;
-    background-color: #47c225 ;
-    box-shadow: 0 2px 10px 1px #297535;
     border: none;
     z-index: 100;
+    ${backgroundStyle}
 `;
